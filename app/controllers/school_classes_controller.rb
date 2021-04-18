af01e24583aa823eebe119daeb8a4486aef0ee17
Christerle Nounoute <christerlesn@gmail.com>
2021-04-18 15:57:07 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,35 +1,33 @@
 class SchoolClassesController < ApplicationController
-  def new
-    @schoolclass = SchoolClass.new
-  end
+def new
+        @school_class = SchoolClass.new
+    end
 
-  def create
-    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
-    @school_class.save
-    redirect_to school_class_path(@school_class)
-  end
+    def create #use strong parameters
+      @school_class = SchoolClass.create(school_class_params(:title, :room_number))
+      redirect_to school_class_path(@school_class)       
+    end
 
-  def show
-    @school_class = SchoolClass.find(params[:id])
-  end
+    def show
+        @school_class = SchoolClass.find(params[:id])
+    end 
 
-  def index
-    @school_classes = SchoolClass.all
-  end
+    def edit
+      @school_class = SchoolClass.find(params[:id])
+    end
 
-  def edit
-    @school_class = SchoolClass.find(params[:id])
-  end
+    def update #use strong parameters
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(school_class_params(:title, :room_number))
+        redirect_to school_class_path(@school_class)
+    end
 
-  def update
-    @school_class = SchoolClass.find(params[:id])
-    @school_class.update(school_class_params(:title, :room_number))
-    redirect_to school_class_path(@school_class)
-  end
+    def index 
+        @school_classes = SchoolClass.all 
+    end
 
-  private
+    private
 
-  def school_class_params(*args)
-    params.require(:school_class).permit(*args)
-  end
-end
+    def school_class_params(*args)
+        params.require(:school_class).permit(*args)
+    end
