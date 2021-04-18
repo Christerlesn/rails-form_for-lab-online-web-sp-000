c5eef6784c084e26225ccb2652e6aa6687612431
Christerle Nounoute <christerlesn@gmail.com>
2021-04-18 15:57:10 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,12 +5,12 @@ def new
 
     def create #use strong parameters
       @school_class = SchoolClass.create(school_class_params(:title, :room_number))
-      redirect_to school_class_path(@school_class)       
+      redirect_to school_class_path(@school_class)
     end
 
     def show
         @school_class = SchoolClass.find(params[:id])
-    end 
+    end
 
     def edit
       @school_class = SchoolClass.find(params[:id])
@@ -22,8 +22,8 @@ def new
         redirect_to school_class_path(@school_class)
     end
 
-    def index 
-        @school_classes = SchoolClass.all 
+    def index
+        @school_classes = SchoolClass.all
     end
 
     private
